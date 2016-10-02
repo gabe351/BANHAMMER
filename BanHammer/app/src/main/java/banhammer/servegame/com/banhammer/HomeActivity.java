993f5b448c131c405d7fefa91f0c2fcba7d113bb
Gaberu <gabrielrosadn@gmail.com>
2016-10-02 03:01:48 -0300

    side menu allmost complete
    Signed-off-by: Gaberu <gabrielrosadn@gmail.com>

--- BanHammer/app/src/main/java/banhammer/servegame/com/banhammer/HomeActivity.java
+++ BanHammer/app/src/main/java/banhammer/servegame/com/banhammer/HomeActivity.java
@@ -1,13 +1,109 @@
 package banhammer.servegame.com.banhammer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.home_toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.home_drawer_layout)
+    DrawerLayout mDrawer;
+
+    @BindView(R.id.navigation_view)
+    NavigationView mNavigationView;
+
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private FragmentTransaction mFragmentTransaction;
+    private MenuItem mPreviousMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_act);
+        ButterKnife.bind(this);
+
+        if (mToolbar != null){
+            setSupportActionBar(mToolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.open, R.string.close);
+        mDrawer.setDrawerListener(mActionBarDrawerToggle);
+
+        mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+        mFragmentTransaction.add(R.id.main_container, new HomeFragment());
+        mFragmentTransaction.commit();
+        getSupportActionBar().setTitle("Home");
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                item.setCheckable(true);
+                item.setChecked(true);
+                if (mPreviousMenuItem != null) {
+                    mPreviousMenuItem.setChecked(false);
+                }
+                mPreviousMenuItem = item;
+
+                switch (item.getItemId()){
+
+                    case R.id.drawer_home:
+                        mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        mFragmentTransaction.replace(R.id.main_container, new HomeFragment());
+                        mFragmentTransaction.commit();
+                        mDrawer.closeDrawers();
+                        break;
+
+                    case R.id.drawer_message:
+                        mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        mFragmentTransaction.replace(R.id.main_container, new MessageFragment());
+                        mFragmentTransaction.commit();
+                        mDrawer.closeDrawers();
+                        break;
+                    case R.id.drawer_whisper:
+                        mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        mFragmentTransaction.replace(R.id.main_container, new WhisperFragment());
+                        mFragmentTransaction.commit();
+                        mDrawer.closeDrawers();
+                        break;
+
+                    case R.id.drawer_kick:
+                        mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        mFragmentTransaction.replace(R.id.main_container, new KickFragment());
+                        mFragmentTransaction.commit();
+                        mDrawer.closeDrawers();
+                        break;
+
+                    case R.id.drawer_ban:
+                        mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        mFragmentTransaction.replace(R.id.main_container, new TrueHammerFragment());
+                        mFragmentTransaction.commit();
+                        mDrawer.closeDrawers();
+                        break;
+
+                }
+
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mActionBarDrawerToggle.syncState();
     }
 }
---
+++ BanHammer/app/src/main/java/banhammer/servegame/com/banhammer/TrueHammerFragment.java
@@ -0,0 +1,25 @@
+package banhammer.servegame.com.banhammer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Gabe on 02/10/2016.
+ */
+public class TrueHammerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.ban_frag, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+}
